b3559f60775b50a95303ff3b940989a4905bc810
Infra-Blue <rudro1152@gmail.com>
2020-12-23 04:10:20 +0600

    improved menu

--- des.cpp
+++ des.cpp
@@ -5,6 +5,7 @@ enum {ENCRYPT, DECRYPT};
 
 std::string des (const std::string& text, const std::string& key, int MODE) {
 
+    return "";
 }
 
 
--- menu.cpp
+++ menu.cpp
@@ -3,5 +3,24 @@ enum {ENCRYPT, DECRYPT};
 
 
 int main () {
-        
+    char choice;
+    std::string text, key;
+
+    std::cout << "< DES Encryption >\nEnter your choice <e to encrypt, d to decrypt>: ";        
+    std::cin >> choice;
+    std::cout << "Enter Text: ";
+    std::cin >> text;
+    std::cout << "Enter Key (Must be 8 alphanumeric characters): ";
+    std::cin >> key;
+    
+    if (key.size() != 8 or (choice != 'e' and choice != 'd')) {
+        std::cout << "<<< ERROR >>>" << std::endl;
+        return 1;
+    }
+    
+    std::cout << std::endl << "text begins |";
+
+    
+    
+    std::cout << "| text ends" << std::endl;
 }
