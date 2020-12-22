f69a499188502d033c40d50a0e51c2b316928c16
Infra-Blue <rudro1152@gmail.com>
2020-12-23 05:24:01 +0600

    made improvements on menu

--- menu.cpp
+++ menu.cpp
@@ -1,26 +1,38 @@
 #include "header.h"
-enum {ENCRYPT, DECRYPT};
+void error_handling ();
+void text_padding ();
+// enum {ENCRYPT, DECRYPT};
+
+
+char choice;
+std::string text, key;
 
 
 int main () {
-    char choice;
-    std::string text, key;
-
+    // user prompts
     std::cout << "< DES Encryption >\nEnter your choice <e to encrypt, d to decrypt>: ";        
     std::cin >> choice;
     std::cout << "Enter Text: ";
     std::cin >> text;
     std::cout << "Enter Key (Must be 8 alphanumeric characters): ";
     std::cin >> key;
+    std::cout << std::endl;
+
+    error_handling();
+    text_padding();    
     
+    std::cout << "text begins |" << des(text, key, choice == 'd') << "| text ends" << std::endl;
+}
+
+
+void error_handling () {
     if (key.size() != 8 or (choice != 'e' and choice != 'd')) {
         std::cout << "<<< ERROR >>>" << std::endl;
-        return 1;
+        exit(EXIT_FAILURE);
     }
-    
-    std::cout << std::endl << "text begins |";
+}
 
-    
-    
-    std::cout << "| text ends" << std::endl;
+void text_padding () {
+   while (text.size() % 8 != 0)
+        text.push_back(' ');
 }
