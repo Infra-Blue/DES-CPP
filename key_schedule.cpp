b9b4241f8c366a318e17cb4e38d96455c897269b
Infra-Blue <rudro1152@gmail.com>
2020-12-23 09:00:58 +0600

    error-prone, committed for safety

--- key_schedule.cpp
+++ key_schedule.cpp
@@ -1,6 +1,14 @@
 #include "header.h"
 enum {ENCRYPT, DECRYPT};
 
+
+void rotate (std::bitset<28> &block) {
+    int rightMost = block[0];
+    block >>= 1;
+    block[27] = rightMost;
+}
+
+
 std::vector<std::bitset<48>> generate_sub_keys (const std::string &key) {
     // converting key-string for bitwise operation
     int i = 0;
@@ -15,11 +23,26 @@ std::vector<std::bitset<48>> generate_sub_keys (const std::string &key) {
     }
 
     // S-tables for PC-1 and PC-2
-    std::vector<int> pc_1_left{ 57, 49,	41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60,	52, 44, 36 },
-                     pc_1_right{ 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4 },
-                     pc_2{ 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40,
-                           51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 };
+    std::vector<int> pc_1_left{ 56, 48, 40, 32, 24, 16, 8, 0, 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35 },
+                     pc_1_right{ 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 60, 52, 44, 36, 28, 20, 12, 4, 27, 19, 11, 3 },
+                     pc_2{ 13, 16, 10, 23, 0, 4, 2, 27, 14, 5, 20, 9, 22, 18, 11, 3, 25, 7, 15, 6, 26, 19, 12, 1, 40, 51, 30, 36, 46, 54, 29, 39, 50, 44, 32, 47, 43, 48, 
+                           38, 55, 33, 52, 45, 41, 49, 35, 28, 31 };
     
+    std::vector<std::bitset<48>> sub_keys(16);
     std::bitset<28> left, right;
+    
+    // generating 48-bit keys
+    for (auto &sk : sub_keys) {
+        for (int i = 0; i < 28; ++i) {
+            left[i] = left[pc_1_left[i]];
+            right[i] = right[pc_1_right[i]];
+        }
+        rotate(left);
+        rotate(right);
+        for (int i = 0; i < 24; ++i) {
+            left[i] = left[pc_2[i]];
+            right[i] = right[pc_2[i + 24]];
+        }
 
+    }
 }
