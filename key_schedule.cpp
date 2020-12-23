e958299a97983bc6eda51f12e8594bbff52e147d
Infra-Blue <rudro1152@gmail.com>
2020-12-23 07:15:36 +0600

    constructed string to bit converter for key

--- key_schedule.cpp
+++ key_schedule.cpp
@@ -1,2 +1,16 @@
 #include "header.h"
 enum {ENCRYPT, DECRYPT};
+
+std::vector<std::bitset<48>> generate_sub_keys (const std::string &key) {
+    // converting key-string for bitwise operation
+    int i = 0;
+    std::bitset<64> bit_key;
+    for (int c : key) {
+        int plate = 1 << 7;
+        while(plate) {
+            bit_key[i] = plate & c;
+            plate >>= 1;
+            ++i;
+        }
+    }
+}
